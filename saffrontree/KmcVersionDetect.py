819b4dc3aecd593d9f9f01b16004525d67ff9a02
andrewjpage <andrewjpage@gmail.com>
2017-03-09 17:08:48 +0000

    Allow for kmc version 2 to also be used

--- saffrontree/KmcVersionDetect.py
+++ saffrontree/KmcVersionDetect.py
@@ -6,6 +6,7 @@ from subprocess import check_output
 '''The syntax has changed between version 2 and 3 so detect which major version it is'''
 class KmcVersionDetect:
 	def __init__(self, verbose):
+		self.logger = logging.getLogger(__name__)
 		self.verbose = verbose
 		self.kmc_version = self.find_version()
 	
@@ -14,10 +15,11 @@ class KmcVersionDetect:
 	'''K-Mer Counter (KMC) ver. 3.0.0 (2017-01-28)'''
 	def find_version(self):	
 		self.logger.info("Detecting the version of KMC" )
-		kmc_output = check_output(["kmc"])
+		kmc_output = check_output(["kmc"], universal_newlines=True)
+
 		version_search_results = re.search("ver\. ([\d]+\.[\d]+\.[\d]+) ", kmc_output)
 		if version_search_results:
-			return search_results.group(1)
+			return version_search_results.group(1)
 		else:
 			return '0.0.0'
 		
@@ -28,10 +30,12 @@ class KmcVersionDetect:
 			return search_results.group(group)
 		else:
 			return 0
-		
+	
+	'''Return just the first number for the version which denotes the major API version'''
 	def major_version(self):
 		return self.__version_search__(1)
-		
+	
+	'''Return the full version number x.y.z'''
 	def full_version(self):
 		return self.__version_search__(0)
 
--- saffrontree/tests/KmcIntersect_test.py
+++ saffrontree/tests/KmcIntersect_test.py
@@ -10,29 +10,44 @@ class TestKmcIntersect(unittest.TestCase):
 	
 	'''Check the intersect command and parameters are put together in a sensible manner'''
 	def test_kmc_intersect_command(self):
-		k = KmcIntersect('first_database','second_database', os.getcwd(), 1, 'results', False  )
+		k = KmcIntersect('first_database','second_database', os.getcwd(), 1, 'results', False, 3 )
 		
 		self.assertEqual(k.kmc_intersect_command(), 'kmc_tools -t1 simple first_database second_database intersect results > /dev/null 2>&1')
 		k.cleanup()
 		
+	'''Check the intersect command for kmc version 2'''
+	def test_kmc_intersect_command_kmc2(self):
+		k = KmcIntersect('first_database','second_database', os.getcwd(), 1, 'results', False, 2 )
+		
+		self.assertEqual(k.kmc_intersect_command(), 'kmc_tools -t1 intersect first_database second_database results > /dev/null 2>&1')
+		k.cleanup()
+		
 	'''Dont suppress the KMC tools output'''
 	def test_kmc_intersect_command_verbose(self):
-		k = KmcIntersect('first_database','second_database', os.getcwd(), 1, 'results', True  )
+		k = KmcIntersect('first_database','second_database', os.getcwd(), 1, 'results', True, 3 )
 		
 		self.assertEqual(k.kmc_intersect_command(), 'kmc_tools -t1 simple first_database second_database intersect results ')
 		k.cleanup()
 
 	'''check the histogram command is correctly constructed'''
 	def test_kmc_histogram_command(self):
-		k = KmcIntersect('first_database','second_database', os.getcwd(), 1, 'results', False  )
+		k = KmcIntersect('first_database','second_database', os.getcwd(), 1, 'results', False, 3 )
 		
 		actual_command = (k.kmc_histogram_command()).replace(k.temp_working_dir,'/path')
 		self.assertEqual(actual_command, 'kmc_tools transform results histogram /path/histogram > /dev/null 2>&1')	
 		k.cleanup()
 		
+	'''check the histogram command for kmc 2'''
+	def test_kmc_histogram_command_kmc2(self):
+		k = KmcIntersect('first_database','second_database', os.getcwd(), 1, 'results', False, 2 )
+		
+		actual_command = (k.kmc_histogram_command()).replace(k.temp_working_dir,'/path')
+		self.assertEqual(actual_command, 'kmc_tools histogram results /path/histogram > /dev/null 2>&1')	
+		k.cleanup()
+		
 	'''check the histogram command can be verbose'''
 	def test_kmc_histogram_command_verbose(self):
-		k = KmcIntersect('first_database','second_database', os.getcwd(), 1, 'results', True  )
+		k = KmcIntersect('first_database','second_database', os.getcwd(), 1, 'results', True, 3 )
 		
 		actual_command = (k.kmc_histogram_command()).replace(k.temp_working_dir,'/path')
 		self.assertEqual(actual_command, 'kmc_tools transform results histogram /path/histogram ')
@@ -43,7 +58,7 @@ class TestKmcIntersect(unittest.TestCase):
 		first_db = first_sample.run().output_database_name()
 		second_sample = KmcFastq(os.getcwd(), os.path.join(data_dir, 'S_typhi_CT18_chromosome_1.fastq.gz'), 1, 10, 10, 200, False )
 		second_db = second_sample.run().output_database_name()
-		k = KmcIntersect(first_db,second_db, os.getcwd(), 1, 'results', False  )
+		k = KmcIntersect(first_db,second_db, os.getcwd(), 1, 'results', False, 3 )
 		
 		self.assertTrue(k.run())
 		self.assertTrue(os.path.exists('results.kmc_pre'))
